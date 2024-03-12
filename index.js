c243f7ef796e95c468276a433f6548982334637b
Danielpatinno <Neniade777@gmail.com>
2024-03-12 12:37:05 -0300

    Update

--- index.js
+++ index.js
@@ -10,22 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 // config cors
-const whiteList = ["https://app-sorteio4.netlify.app", 'http://localhost:5173']
+app.use(cors({credentials:true, origin:"http://localhost:5173"}))
 
-const corsOptions = {
-  credentials: true,
-  origin: function(origin, callback) {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions))
-
-// app.use(cors({credentials:true, origin:"https://app-sorteio4.netlify.app"}))
+// "https://app-sorteio4.netlify.app"
 
 // rotas
 const router = require("./routes/Router")
