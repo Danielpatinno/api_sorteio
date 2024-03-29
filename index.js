8a5f54433bd966827e747e8def0033e2b669ee7a
Danielpatinno <Neniade777@gmail.com>
2024-03-29 11:57:52 -0300

    ajustes api

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require("path");
 const cors = require("cors")
 
 const app = express()
@@ -12,9 +11,10 @@ app.use(express.urlencoded({extended:false}))
 
 // config cors
 app.use(cors({credentials:true, origin:"https://rifa-tupperware.netlify.app"}))
+// app.use(cors({credentials:true, origin:"http://localhost:5173"}))
 
 // uploads
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 // rotas
 const router = require("./routes/Router")
--- routes/Router.js
+++ routes/Router.js
@@ -1,21 +1,45 @@
 const express = require('express')
-const { registerAdm, login, updateAdm, getAdmById, deleteAdm } = require('../controllers/AdmController')
 const router = express()
 
-const { buyNumbers, getClients, deleteClient, getClientById, updateClient, deleteAllClients } = require("../controllers/ClientController")
-const { registerRifa, getRifa, deleteImageRifa } = require('../controllers/RifaController')
-const { admCreateValidation, admLoginValidation, admUpdateValidation } = require('../middlewares/admValidation')
-const validate = require('../middlewares/handleValidation')
-const { imageUpload } = require('../middlewares/imageUploads')
-const { insertRifaValidation } = require('../middlewares/rifaValidation')
+const {
+  upload,
+  deleteImageMais,
+  registerImageMais,
+  getImageMais
+} = require("../controllers/ImageController")
 
+const {
+  registerAdm,
+  login,
+  updateAdm,
+  getAdmById,
+  deleteAdm
+} = require('../controllers/AdmController')
+
+const {
+  buyNumbers,
+  getClients,
+  deleteClient,
+  getClientById,
+  updateClient,
+  deleteAllClients
+} = require("../controllers/ClientController")
+
+const {
+  admCreateValidation,
+  admLoginValidation,
+  admUpdateValidation
+} = require('../middlewares/admValidation')
+
+const validate = require('../middlewares/handleValidation')
+
+// Clients
 router.post('/create', buyNumbers)
 router.get('/clients', getClients)
 router.delete('/delete/:id', deleteClient)
 router.get('/clientDetails/:id', getClientById)
 router.put('/clientUpdate/:id', updateClient)
 router.delete('/deleteClients', deleteAllClients)
-
 
 // Adm
 router.post('/registerAdm',admCreateValidation(), validate, registerAdm)
@@ -24,9 +48,9 @@ router.put('/updateAdm/:id', admUpdateValidation(), validate, updateAdm)
 router.get("/adm/:id", getAdmById)
 router.delete("/deleteAdm/:id", deleteAdm)
 
-// Rifa
-router.post('/addRifa', imageUpload.single('rifaImage'), validate,insertRifaValidation(), registerRifa )
-router.get('/getRifa', getRifa)
-router.delete('/deleteRifa', deleteImageRifa)
+// Image
+router.post('/registerImage', upload.single('image'), registerImageMais)
+router.get('/getImage', getImageMais)
+router.delete('/deleteImage', deleteImageMais)
 
 module.exports = router
